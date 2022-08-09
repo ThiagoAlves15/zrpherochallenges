7f2024fcee0a4c59124f29acb7a44afdc35068fa
ThiagoAlves15 <thiago.o.alves@hotmail.com>
2022-08-09 14:58:47 -0300

    Add socket io for ocurrence polling

--- onu_painel/src/app/feature/OnuBoard.jsx
+++ onu_painel/src/app/feature/OnuBoard.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { io } from "socket.io-client";
+import { OCURRENCES_URL } from '../api/consts';
+
+const socket = io(OCURRENCES_URL);
 
 function OnuBoard() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastOcurrence, setLastOcurrence] = useState(null);
+
   const currentUser = useSelector((state) => state.session.currentUser);
   const accessToken = useSelector((state) => state.session.accessToken);
   const refreshToken = useSelector((state) => state.session.refreshToken);
 
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log(socket.id);
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', (reason) => {
+      console.log(reason);
+      setIsConnected(false);
+    });
+
+    socket.on('ocurrence', data => {
+      setLastOcurrence(data);
+    });
+
+    socket.on('connect_error', (error) => {
+      console.log(error);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('ocurrence');
+      socket.off('connect_error');
+    };
+  }, []);
+
   return (
     <section>
       <h1>OnuBoard</h1>
+
+      <p>Connected: { '' + isConnected }</p>
+      <p>Last ocurrence: { lastOcurrence || '-' }</p>
 
       <ul>
         <li>Current User
